3987e60ee8b4606679f9ad4be6057f5140bdcb5d
窗外浮冰 <110780208+KanFuBing@users.noreply.github.com>
2022-08-12 10:48:10 +0800

    Uniformed code style.

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     const splitUrl = tab.url.split('/')
     if (changeInfo.status === 'complete' && splitUrl[2] === 'twitter.com' && splitUrl[4] == 'status') {
         chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["./foreground.js"]
+            target: { tabId },
+            files: ['./foreground.js']
         })
     }
 })
